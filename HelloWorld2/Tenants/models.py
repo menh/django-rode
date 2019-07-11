f7e5eff293900a52d8d6dd72495d0de42f272b99
menhu <872204778@qq.com>
2019-07-11 18:27:38 +0800

    d

---
+++ HelloWorld2/Tenants/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from tenant_schemas.models import TenantMixin
+# Create your models here.
+
+class Client(TenantMixin):
+    name = models.CharField(max_length = 100)
+    paid_until = models.DateField()
+    on_trial = models.BooleanField()
+    created_on = models.DeteField(auto_now_add = True)
+    auto_create_schema = True
---
+++ HelloWorld2/Tenants/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+from django.db import models
+from tenant_schemas.models import TenantMixin
+# Create your tests here.
